28c39b67947b55d06711a66d3cc96714e8904b2c
phyominpaing <minpaingphyo255@gmail.com>
2026-04-04 13:37:05 +0630

    client/project init and base template

---
+++ client/src/layouts/Main.tsx
@@ -0,0 +1,9 @@
+import { Outlet } from 'react-router'
+
+const Main = () => {
+  return (
+   <Outlet/>
+  )
+}
+
+export default Main
---
+++ client/src/main.tsx
@@ -0,0 +1,25 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Main from "./layouts/Main.tsx";
+import Home from "./pages/Home.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+);
