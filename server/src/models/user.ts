2d2e901f96da6e00d348a527db2093d8089d43bc
phyominpaing <minpaingphyo255@gmail.com>
2026-04-07 22:59:02 +0630

    server/user model and middleware setup

---
+++ server/src/models/user.ts
@@ -0,0 +1,32 @@
+import mongoose, { Document, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: "customer" | "admin";
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["customer", "admin"] },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return;
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword: string) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export const User = mongoose.model<IUser>("User", userSchema);
