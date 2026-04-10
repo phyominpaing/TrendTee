25476358f38e81c775fe26102e93f0e5f6c44e3d
phyominpaing <minpaingphyo255@gmail.com>
2026-04-10 23:09:25 +0630

    server/update and delete product

--- server/src/controllers/product.ts
+++ server/src/controllers/product.ts
@@ -3,6 +3,9 @@ import asyncHandler from "../utils/asyncHandler.ts";
 import { Product } from "../models/product.ts";
 import type { AuthRequest } from "../middlewares/authMiddleware.ts";
 
+// @route POST - api/products
+// @desc Create a new product
+// @access Private
 export const createProduct = asyncHandler(
   async (req: AuthRequest, res: Response) => {
     const {
@@ -37,10 +40,89 @@ export const createProduct = asyncHandler(
     if (newProduct) {
       res.status(201).json({
         message: `Product ${newProduct.name} created successfully.`,
+        product: newProduct,
       });
     } else {
       res.status(400);
       throw new Error("Failed to create product.");
     }
+  },
+);
+
+// @route PUT - api/products/:id
+// @desc Update an existing product
+// @access Private
+export const updateProduct = asyncHandler(
+  async (req: AuthRequest, res: Response) => {
+    const {
+      name,
+      description,
+      price,
+      instock_count,
+      category,
+      sizes,
+      colors,
+      images,
+      is_new_arrival,
+      is_feature,
+      rating_count,
+    } = req.body;
+
+    const { id } = req.params;
+
+    const existingProduct = await Product.findById(id);
+
+    if (!existingProduct) {
+      res.status(404);
+      throw new Error("Product not found.");
+    }
+
+    existingProduct.name = name || existingProduct.name;
+    existingProduct.description = description || existingProduct.description;
+    existingProduct.price = price || existingProduct.price;
+    existingProduct.instock_count =
+      instock_count || existingProduct.instock_count;
+    existingProduct.category = category || existingProduct.category;
+    existingProduct.sizes = sizes || existingProduct.sizes;
+    existingProduct.colors = colors || existingProduct.colors;
+    existingProduct.images = images || existingProduct.images;
+    existingProduct.is_new_arrival =
+      is_new_arrival || existingProduct.is_new_arrival;
+    existingProduct.is_feature = is_feature || existingProduct.is_feature;
+    existingProduct.rating_count = rating_count || existingProduct.rating_count;
+
+    const updatedProduct = await existingProduct.save();
+
+    if (updatedProduct) {
+      res.status(200).json({
+        message: `Product ${updatedProduct.name} updated successfully.`,
+        product: updatedProduct,
+      });
+    } else {
+      res.status(400);
+      throw new Error("Failed to update product.");
+    }
+  },
+);
+
+// @route DELETE - api/products/:id
+// @desc Delete an existing product
+// @access Private/Admin
+export const deleteProduct = asyncHandler(
+  async (req: AuthRequest, res: Response) => {
+    const { id } = req.params;
+
+    const existingProduct = await Product.findById(id);
+
+    if (!existingProduct) {
+      res.status(404);
+      throw new Error("Product not found.");
+    }
+
+    await existingProduct.deleteOne();
+
+    res.status(200).json({
+      message: `Product ${existingProduct.name} deleted successfully.`,
+    });
   },
 );
--- server/src/routes/product.ts
+++ server/src/routes/product.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { createProduct } from "../controllers/product.ts";
+import { createProduct, deleteProduct, updateProduct } from "../controllers/product.ts";
 import { isAdmin, protect } from "../middlewares/authMiddleware.ts";
 
 const router = Router();
 
 router.post("/products", protect, isAdmin,  createProduct);
+router.put("/products/:id", protect, isAdmin,  updateProduct);
+router.delete("/products/:id", protect, isAdmin,  deleteProduct);
 
 export default router;
