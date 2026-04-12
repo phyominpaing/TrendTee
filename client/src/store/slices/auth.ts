f04c69462a85a76eecd7d791d0e6ac381600edf2
phyominpaing <minpaingphyo255@gmail.com>
2026-04-12 11:30:41 +0630

    client/redux toolkit setup

---
+++ client/src/store/slices/auth.ts
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface AuthState {
+  userInfo: {
+    _id: string;
+    email: string;
+    username: string;
+  } | null;
+}
+
+const initialState: AuthState = {
+  userInfo: localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo") as string)
+    : null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUserInfo: (state, action) => {
+      ((state.userInfo = action.payload),
+        localStorage.setItem("userInfo", JSON.stringify(action.payload)));
+    },
+    clearUserInfo: (state) => {
+      ((state.userInfo = null), localStorage.removeItem("userInfo"));
+    },
+  },
+});
+
+export const { setUserInfo, clearUserInfo } = authSlice.actions;
+export default authSlice.reducer;
