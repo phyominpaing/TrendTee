2e73e69401cc7e9c157ee040f1e2a202538af0eb
phyominpaing <minpaingphyo255@gmail.com>
2026-05-08 23:42:06 +0630

    client/product form inputs schema added

---
+++ client/src/schema/product.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+const IMAGES_TYPE = ["image/jpeg", "image/png", "image/jpg", "image/webp"];
+
+export const productSchema = z.object({
+  name: z.string().min(3, "Product name must be at least 3 characters"),
+  description: z
+    .string()
+    .min(10, "Product description must be at least 10 characters"),
+  price: z.number().min(0, "Price must be greater than 0"),
+  instock_count: z.number(),
+  category: z.string().min(1, "Category is required"),
+  sizes: z
+    .array(z.enum(["xs,sm,lg,xl,xxl"]))
+    .min(1, "At least one size must be selected"),
+  colors: z.array(z.string()).min(1, "At least one color must be selected"),
+  images: z
+    .array(
+      z.object({
+        file: z.instanceof(File).optional(),
+        preview: z.string(),
+        public_alt: z.string().optional(),
+      }),
+    )
+    .min(1, "At least one image URL must be provided"),
+  is_new_arrival: z.boolean(),
+  is_feature: z.boolean(),
+  rating_count: z.number().min(0),
+});
+
+export type ProductFormInputs = z.infer<typeof productSchema>;
