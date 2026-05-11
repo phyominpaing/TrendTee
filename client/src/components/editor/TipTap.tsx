bd971e576e48207cfd42f3cd07a218bbf6326827
phyominpaing <minpaingphyo255@gmail.com>
2026-05-11 23:46:37 +0630

    client/product description form with tiptap

--- client/src/components/editor/TipTap.tsx
+++ client/src/components/editor/TipTap.tsx
@@ -1,9 +1,11 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { Button } from "../ui/button";
+import { Bold, Italic, List, ListOrdered } from "lucide-react";
 
 interface TipTapProps {
-    value : string;
-    onChange : (value : string) => void;
+  value: string;
+  onChange: (value: string) => void;
 }
 const Tiptap = ({ value, onChange }: TipTapProps) => {
   const editor = useEditor({
@@ -15,14 +17,50 @@ const Tiptap = ({ value, onChange }: TipTapProps) => {
     },
   });
 
-  if(!editor) {
+  if (!editor) {
     return null;
   }
 
   return (
     <div className="border rounded-md p-2">
-      <EditorContent editor={editor} className=" prose"/>
-     
+      <div className="flex items-center gap-2 mb-2">
+        <Button
+          type="button"
+          size={"sm"}
+          variant={editor.isActive("bold") ? "default" : "outline"}
+          onClick={() => editor.chain().focus().toggleBold().run()}
+        >
+          <Bold className="h-4 w-4" />
+        </Button>
+
+        <Button
+          type="button"
+          size={"sm"}
+          variant={editor.isActive("italic") ? "default" : "outline"}
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+        >
+          <Italic className="h-4 w-4" />
+        </Button>
+
+        <Button
+          type="button"
+          size={"sm"}
+          variant={editor.isActive("bulletList") ? "default" : "outline"}
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+        >
+          <List className="h-4 w-4" />
+        </Button>
+
+        <Button
+          type="button"
+          size={"sm"}
+          variant={editor.isActive("orderedList") ? "default" : "outline"}
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+        >
+          <ListOrdered className="h-4 w-4" />
+        </Button>
+      </div>
+      <EditorContent editor={editor} className=" prose" />
     </div>
   );
 };
