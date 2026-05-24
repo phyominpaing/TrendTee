a0287831ea7dd6567b2bcfe271de232e42c97517
phyominpaing <minpaingphyo255@gmail.com>
2026-05-24 22:26:17 +0630

    server/stripe session url create controller

---
+++ server/src/controllers/order.ts
@@ -0,0 +1,49 @@
+import type { Request, Response } from "express";
+import asyncHandler from "../utils/asyncHandler.ts";
+import type { AuthRequest } from "../middlewares/authMiddleware.ts";
+import Stripe from "stripe";
+import type { OrderItem } from "../models/order.ts";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2026-04-22.dahlia",
+});
+
+// @route GET - api/create-order
+// @desc Add new order and request stripe section
+// @access Private/User
+const createOrderAndCheckOutSession = asyncHandler(
+  async (req: AuthRequest, res: Response) => {
+    const { items, bill } = req.body;
+    const customerId = req.user?._id;
+
+    const line_items = items.map((item: OrderItem) => ({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name,
+          metaData: {
+            productId: item.productId,
+            color: item.color,
+            size: item.size,
+          },
+        },
+      },
+      quantity: item.quantity,
+    }));
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items,
+      mode: "payment",
+      success_url: `${process.env.CLIENT_URL}/order-success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.CLIENT_URL}/order-cancelled`,
+      metadata: {
+        customerId: customerId?.toString()!,
+        cart: JSON.stringify(items),
+        bill: bill.toString(),
+      },
+    });
+
+    res.status(200).json({ url: session.url });
+  },
+);
